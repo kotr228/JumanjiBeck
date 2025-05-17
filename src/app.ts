382050539723a03413974d057231c5f2d64e7a63
kotr228 <trockijkostantin@gmail.com>
2025-05-17 08:29:48 +0300

    favorites

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import authRoutes from './routes/auth';        // ✅ маршрут авторизації/реєстрації
-import userRoutes from './routes/userRoutes';  // інші запити (якщо в тебе меню/кошик тощо)
+import authRoutes from './routes/auth';       
+import userRoutes from './routes/userRoutes';  
+import favoritesRoutes from './routes/favorites';
 
 const app = express();
 
-// 👇 Дозволити запити з фронтенду
+
 app.use(cors({
   origin: 'http://localhost:5173', // адаптуй якщо у тебе інша адреса
   credentials: true
@@ -15,11 +16,10 @@ app.use(cors({
 app.use(express.json());  // для обробки JSON-запитів
 app.use(bodyParser.urlencoded({ extended: true })); // (опціонально, якщо приймаєш form-data)
 
-// ✅ Додаємо маршрути
-app.use('/api/users', userRoutes); // напр. /api/users/profile
-app.use('/api/auth', authRoutes);  // 🔑 /api/auth/register та інші
+app.use('/api/users', userRoutes); 
+app.use('/api/auth', authRoutes);
+app.use('/api/favorites', favoritesRoutes); 
 
-// 🚀 Запуск сервера
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('🚀✅Server is running on port 3000');
 });
