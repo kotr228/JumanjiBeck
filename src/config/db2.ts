cbe748786766ffe672a31f144d0d4bd80d46ec03
kotr228 <trockijkostantin@gmail.com>
2026-01-20 10:12:34 +0200

    зміна дб

--- src/config/db2.ts
+++ src/config/db2.ts
@@ -1,10 +1,15 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'nkloqzcz_kote',
-  password: 'Sillver-228',
-  database: 'nkloqzcz_Jumanji',
+  host: 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
+  user: 'v73fwwxNCgoBNeM.root',
+  password: 'LsdrxxCC8bCUb5BQ',
+  database: 'test',
+  port: 4000,       // <--- ВАЖЛИВО: Для TiDB це обов'язково
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
